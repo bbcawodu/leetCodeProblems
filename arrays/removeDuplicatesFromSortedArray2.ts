1a254f0a55596b5776750127038c574e6a7e9f9c
Bradley Awodu <awodubradley@gmail.com>
2026-03-30 10:03:25 -0500

    add remove duplicates 2

---
+++ arrays/removeDuplicatesFromSortedArray2.ts
@@ -0,0 +1,74 @@
+/*
+Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3]
+Output: 5, nums = [1,1,2,2,3,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,1,2,3,3]
+Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-104 <= nums[i] <= 104
+nums is sorted in non-decreasing order.
+*/
+
+function removeDuplicates(nums: number[]): number {
+    let newElementIndex = 1;
+    let compareIndex = 1;
+
+    while (compareIndex < nums.length) {
+        if (
+            nums[newElementIndex - 1] !== nums[compareIndex] ||
+            nums[newElementIndex - 2] !== nums[compareIndex]
+        ) {
+            nums[newElementIndex] = nums[compareIndex];
+            newElementIndex++;
+        }
+
+        compareIndex++;
+    }
+
+    return newElementIndex;
+};
+
+let nums = [1,1,1,2,2,3];
+console.log(removeDuplicates(nums), nums); // 5, [1,1,2,2,3,_]
+
+nums = [0,0,1,1,1,1,2,3,3];
+console.log(removeDuplicates(nums), nums); // 7, [0,0,1,1,2,3,3,_,_]
+
+nums = [1,1,1,1,1,1];
+console.log(removeDuplicates(nums), nums); // 2, [1,1,_,_,_,_]
