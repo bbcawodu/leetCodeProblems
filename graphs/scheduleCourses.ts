bfc46efee2aac732532bef393ca95e766222920c
Bradley Awodu <awodubradley@gmail.com>
2026-04-01 13:14:29 -0500

    add schedule courses

---
+++ graphs/scheduleCourses.ts
@@ -0,0 +1,101 @@
+/**
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return true if you can finish all courses. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0. So it is possible.
+Example 2:
+
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+ 
+
+Constraints:
+
+1 <= numCourses <= 2000
+0 <= prerequisites.length <= 5000
+prerequisites[i].length == 2
+0 <= ai, bi < numCourses
+All the pairs prerequisites[i] are unique.
+ */
+
+function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+    const courseGraph = buildGraph(numCourses, prerequisites);
+
+    const ordering = runKahns(courseGraph);
+
+    return ordering.length === numCourses;
+};
+
+function buildGraph(numCourses: number, prerequisites: number[][]): Record<number, number[]> {
+    const graph: Record<number, number[]> = {};
+
+    for (let i = 0; i < numCourses; i++) {
+        graph[i] = [];
+    }
+
+    for (let [course, prerequisite] of prerequisites) {
+        graph[prerequisite].push(course);
+    }
+
+    return graph;
+}
+
+function runKahns(courseGraph: Record<number, number[]>): number[] {
+    const indegrees: Record<number, number> = {};
+    for (let node in courseGraph) {
+        indegrees[node] = 0;
+    }
+    for (let node in courseGraph) {
+        for (let neighbor of courseGraph[node]) {
+            indegrees[neighbor]++;
+        }
+    }
+
+    const traversalQueue: number[] = [];
+    for (let node in courseGraph) {
+        if (indegrees[node] === 0) {
+            traversalQueue.push(node as unknown as number);
+        }
+    }
+
+    let ordering: number[] = [];
+    while (traversalQueue.length > 0) {
+        let top = traversalQueue.shift();
+        ordering.push(top as unknown as number);
+
+        for (let neighbor of courseGraph[top as unknown as number]) {
+            indegrees[neighbor]--;
+            if (indegrees[neighbor] === 0) {
+                traversalQueue.push(neighbor);
+            }
+        }
+    }
+
+    return ordering;
+}
+
+let numCourses = 2, prerequisites = [[1,0]];
+console.log(canFinish(numCourses, prerequisites)); // true
+
+numCourses = 2, prerequisites = [[1,0],[0,1]];
+console.log(canFinish(numCourses, prerequisites)); // false
+
+numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]];
+console.log(canFinish(numCourses, prerequisites)); // true
+
+numCourses = 3, prerequisites = [[0,1],[0,2],[1,2]];
+console.log(canFinish(numCourses, prerequisites)); // true
+
+numCourses = 3, prerequisites = [[0,1],[1,2],[2,0]];
+console.log(canFinish(numCourses, prerequisites)); // false
